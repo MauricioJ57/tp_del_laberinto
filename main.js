07ecb8bf27acf361d1ab339f93fecf0c0545a703
@mauricio-jacki <mauriciojacki@gmail.com>
2025-05-27 16:15:00 -0300

    agrego el mapa de tiled

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import Game from ".scenes/Game.js";
+import laberinto from "./scenes/laberinto.js";
 
 const config = {
   type: Phaser.AUTO,
@@ -26,7 +26,7 @@ const config = {
    // List of scenes to load
   // Only the first scene will be shown
   // Remember to import the scene before adding it to the list
-  scene: [Game],
+  scene: [laberinto],
 };
 
 // Create a new Phaser game instance
