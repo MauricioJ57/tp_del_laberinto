09d7dbbc5d5102b60540e7f85b1daabec798d8b1
@mauricio-jacki <mauriciojacki@gmail.com>
2025-06-01 23:51:34 -0300

    se agregaron nuevos archivos

--- scenes/laberinto.js
+++ scenes/laberinto.js
@@ -1,17 +1,25 @@
 export default class laberinto extends Phaser.Scene {
     constructor() {
-        super("laberinto");
+        super("laberintojuego");
     }
 
     init() {
     }
 
     preload() {
-        this.load.tilemapTiledJSON
-        this.load.image
+        this.load.tilemapTiledJSON("mapa_de_laberinto", "public/assets/tilemap/mapa_de_laberinto.json");
+        this.load.image("background", "public/assets/tilemap-backgrounds.png");
+        this.load.image("tiles", "public/assets/tilemap_packed.png");
+        this.load.image("cuadrado", "public/assets/cuadrado_de_laberinto.png");
     }
 
     create() {
+        const map = this.make.tilemap({ key: "mapa_de_laberinto" });
+        const tileset = map.addTilesetImage("laberinto", "background","decoraciones");
+        const laberintoLayer = map.createLayer("laberinto", tileset, 0, 0);
+
+        this.player = this.physics.add.sprite(50, 50, "cuadrado");
+        
         this.cursors = this.input.keyboard.createCursorKeys();
 
     }
